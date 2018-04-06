1086f5d6e950fa40bba1c0b6bdfe473dec3c42e0
Eldo Jr <eldojr90@gmail.com>
2018-04-05 21:58:52 -0300

    alts

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Pequena aplicação web para gerenciamento de comissões.
 Desenvolvimento em PHP.
 
 >Script criação do bd (MySQL): 'app/Aux/eliana.sql'
+>Alterar dados de acesso ao bd em: 'app/Aux/Connection.sql', método 'argsData()'
 
 
 
--- app/Aux/Connection.php
+++ app/Aux/Connection.php
@@ -27,23 +27,9 @@ class Connection{
 
     public static function argsData(){
 
-        $dbname = "id4828640_eliana";
-        $username = "id4828640_eldo";
-        $password = "elisa010612";
-
-        if($_SERVER["SERVER_NAME"]=="localhost" 
-        || $_SERVER["SERVER_NAME"]=="eldo-junior" 
-        || $_SERVER["SERVER_NAME"]=="ELDO-JUNIOR" 
-        || $_SERVER["SERVER_NAME"]=="127.0.0.1"
-        || $_SERVER["SERVER_NAME"]=="192.168.0.20"
-        || $_SERVER["SERVER_NAME"]=="192.168.1.4"
-        || $_SERVER["SERVER_NAME"]=="192.168.25.94"){
-
-            $dbname = "eliana";
-            $username = "root";
-            $password = "";
-            
-        }
+        $dbname = "";
+        $username = "";
+        $password = "";
 
         return [
             'dsn'=>'mysql:host=localhost;dbname='.$dbname.';',
